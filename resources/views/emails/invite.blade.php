95bc51e7419876e8849e63e41090ab9117dcbd35
mussab <muhammadmussab156@gmail.com>
2024-10-06 10:10:27 +0500

    download app

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -3,7 +3,7 @@
 
 You've been invited to join NeoVista – a new and exciting chat experience. Join in to connect with friends, share moments, and stay in touch effortlessly.
 
-@component('mail::button', ['url' => ''])
+@component('mail::button', ['url' => 'https://drive.google.com/file/d/1NH9n_htgYXCKMotn_Fe50FDocBLEbf1e/view?usp=sharing'])
 Download Now
 @endcomponent
 
